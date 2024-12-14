a8e08caf2cbf3b1b4842a158851e037fdf4758af
macmade <macmade@xs-labs.com>
2024-12-14 18:32:28 +0100

    feat(Watch): Application in progress...

--- SDO-Watch/Views/ContentView.swift
+++ SDO-Watch/Views/ContentView.swift
@@ -26,13 +26,108 @@ import SwiftUI
 
 struct ContentView: View
 {
+    @State private var images: [ ImageData ] = []
+    @State private var lastRefresh           = Date()
+    @State private var isLoading             = false
+
     var body: some View
     {
-        VStack
+        ZStack
         {
-            Text( "hello, world" )
+            if self.images.isEmpty, self.isLoading == false
+            {
+                VStack( spacing: 10 )
+                {
+                    Image( systemName: "sun.max.trianglebadge.exclamationmark.fill" )
+                        .resizable()
+                        .frame( width: 25, height: 25 )
+                        .padding()
+                    Button( "Refresh" )
+                    {
+                        self.refresh()
+                    }
+                }
+                .padding()
+            }
+            else if self.images.isEmpty == false
+            {
+                ScrollView
+                {
+                    VStack( spacing: 0 )
+                    {
+                        Button()
+                        {
+                            self.refresh()
+                        }
+                        label:
+                        {
+                            Label( "Refresh", systemImage: "arrow.trianglehead.2.counterclockwise" )
+                        }
+                        .buttonStyle( .borderless )
+
+                        ForEach( self.images, id: \.uuid )
+                        {
+                            if let image = $0.image
+                            {
+                                Image( uiImage: image )
+                                    .resizable()
+                                    .aspectRatio( contentMode: .fill )
+                                Text( $0.title )
+                                    .font( .caption2 )
+                                    .bold()
+                                    .multilineTextAlignment( .center )
+                                    .padding( 10 )
+                            }
+                        }
+                    }
+                }
+                .blur( radius: self.isLoading ? 75 : 0 )
+                .disabled( self.isLoading )
+            }
+
+            if self.isLoading
+            {
+                VStack
+                {
+                    ProgressView()
+                        .scaleEffect( 1.5 )
+                        .padding()
+                    Text( "Loading Images" )
+                        .foregroundStyle( .secondary )
+                        .multilineTextAlignment( .center )
+                }
+            }
         }
-        .padding()
+        .onAppear
+        {
+            self.refresh()
+        }
+    }
+
+    private func refresh()
+    {
+        self.isLoading = true
+
+        SDO.shared?.downloadAll
+        {
+            images in DispatchQueue.main.asyncAfter( deadline: .now() + .milliseconds( 500 ) )
+            {
+                self.lastRefresh = Date()
+                self.images      = images.filter { $0.image != nil }
+                self.isLoading   = false
+            }
+        }
+    }
+
+    private var lastRefreshText: String
+    {
+        let formatter = DateFormatter()
+
+        formatter.dateStyle                  = .short
+        formatter.timeStyle                  = .short
+        formatter.doesRelativeDateFormatting = true
+
+        return formatter.string( from: self.lastRefresh )
     }
 }
 
